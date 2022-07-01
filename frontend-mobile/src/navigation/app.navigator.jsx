035c59a66b706d893942fd9c92110ab94ecbe3e6
uf-code <ugur.firat.developer@gmail.com>
2022-07-01 23:08:32 +0300

    changing the icon colors

--- frontend-mobile/src/navigation/app.navigator.jsx
+++ frontend-mobile/src/navigation/app.navigator.jsx
@@ -20,18 +20,15 @@ const createScreenOptions = ({ route }) => {
   const iconName = TAB_ICON[route.name]
   return {
     tabBarIcon: ({ size, color }) => <Ionicons name={iconName} size={size} color={color} />,
+    tabBarActiveTintColor: '#F15412',
+    tabBarInactiveTintColor: 'gray',
+    tabBarStyle: [{ display: 'flex' }, null],
     headerShown: false,
   }
 }
 
 export const AppNavigator = () => (
-  <Tab.Navigator
-    screenOptions={createScreenOptions}
-    tabBarOptions={{
-      activeTintColor: '#47B5FF',
-      inactiveTintColor: 'gray',
-    }}
-  >
+  <Tab.Navigator screenOptions={createScreenOptions}>
     <Tab.Screen name='Customers' component={CustomersScreen} />
     <Tab.Screen name='Creator' component={CreatorScreen} />
   </Tab.Navigator>
