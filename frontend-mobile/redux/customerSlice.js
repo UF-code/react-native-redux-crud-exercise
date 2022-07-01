9549bff65558589b99723508ff22049593ce51e0
UF-code <ugur.firat.developer@gmail.com>
2022-07-01 18:49:50 +0300

    implementing redux into react-native

---
+++ frontend-mobile/redux/customerSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const customerSlice = createSlice({
+  name: 'customers',
+  initialState: {
+    customers: [],
+  },
+  reducers: {
+    addCustomers: (state, action) => {
+      state.customers = [...state.customers, action.payload]
+    },
+    getCustomers: (state, action) => {
+      state.customers = [...action.payload]
+    },
+    editCustomers: (state, action) => {
+      state.customers.find((customer) => customer.id === action.payload.id).first_name = action.payload.first_name
+      state.customers.find((customer) => customer.id === action.payload.id).last_name = action.payload.last_name
+      state.customers.find((customer) => customer.id === action.payload.id).email = action.payload.email
+      state.customers.find((customer) => customer.id === action.payload.id).birthdate = action.payload.birthdate
+
+      state.customers = [...state.customers]
+    },
+    deleteCustomer: (state, action) => {
+      state.customers = [...state.customers.filter((customer) => customer.id !== action.payload)]
+    },
+  },
+})
+
+export const { addCustomer, getCustomers, editCustomers, deleteCustomer } = customerSlice.actions
+
+export default customerSlice.reducer
---
+++ frontend-mobile/redux/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit'
+import customerReducer from './customerSlice'
+
+export const store = configureStore({
+  reducer: { customers: customerReducer },
+})
