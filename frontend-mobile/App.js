877939b234674bd388e46a6f9d333973657b080a
UF-code <ugur.firat.developer@gmail.com>
2022-07-01 19:07:53 +0300

    changing the workstation

--- frontend-mobile/App.js
+++ frontend-mobile/App.js
@@ -1,16 +1,23 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-import { Provider } from 'react-redux'
+
+//
+import { Provider as ReduxProvider } from 'react-redux'
 import { store } from './redux/store'
+
+//
+import { Provider as PaperProvider } from 'react-native-paper'
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style='auto' />
-      </View>
-    </Provider>
+    <ReduxProvider store={store}>
+      <PaperProvider>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style='auto' />
+        </View>
+      </PaperProvider>
+    </ReduxProvider>
   )
 }
 
