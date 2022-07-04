d042917c653239f90ac0bf19d89aa7fab1eede60
uf-code <ugur.firat.developer@gmail.com>
2022-07-04 20:31:11 +0300

    implementing card styles component with styled components

--- frontend-mobile/src/components/table.component.jsx
+++ frontend-mobile/src/components/table.component.jsx
@@ -6,18 +6,23 @@ import { Text, View, FlatList, ScrollView } from 'react-native'
 // Redux
 import { useSelector } from 'react-redux'
 
+//
+import { ViewScreenComponent, ViewCardComponent } from '../styled-components/view.component'
+
 export const TableComponent = () => {
   const customers = useSelector((state) => state.customers.customers)
 
   return (
-    <ScrollView>
-      {customers.map((customer) => (
-        <View key={customer.id}>
-          <Text>{`${customer.first_name} ${customer.last_name}`}</Text>
-          <Text>{customer.email}</Text>
-          <Text>{customer.birthdate}</Text>
-        </View>
-      ))}
-    </ScrollView>
+    <ViewScreenComponent>
+      <ScrollView>
+        {customers.map((customer) => (
+          <ViewCardComponent key={customer.id}>
+            <Text>{`${customer.first_name} ${customer.last_name}`}</Text>
+            <Text>{customer.email}</Text>
+            <Text>{customer.birthdate}</Text>
+          </ViewCardComponent>
+        ))}
+      </ScrollView>
+    </ViewScreenComponent>
   )
 }
---
+++ frontend-mobile/src/styled-components/view.component.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native'
+
+export const ViewScreenComponent = styled.View`
+  width: 90%;
+  background-color: red;
+`
+
+export const ViewCardComponent = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2%;
+  border-radius: 20px;
+  background-color: pink;
+`
